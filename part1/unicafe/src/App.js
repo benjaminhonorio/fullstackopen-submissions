6c684216f91feb7a17be9c1a9a193a28a94ec71d
benjaminhonorio <benjaminhonorio.12@gmail.com>
2021-12-09 23:01:06 -0500

    Unicafe step3 and step4 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,29 @@
 import { useState } from "react";
 
+const Statistics = ({ feedback }) => {
+  let { good, neutral, bad } = feedback;
+  let all = good + neutral + bad;
+  if (all !== 0) {
+    let nonZeroSum = all === 0 ? 1 : all;
+    let positive = good / nonZeroSum;
+    return (
+      <>
+        <Result text={"good"} total={good} />
+        <Result text={"neutral"} total={neutral} />
+        <Result text={"bad"} total={bad} />
+        <Result text={"all"} total={all} />
+        <Result
+          text={"all"}
+          total={(good * 1 + neutral * 0 + bad * -1) / nonZeroSum}
+        />
+        <Result text={"positive"} total={positive} />
+      </>
+    );
+  } else {
+    return <div>No feedback given</div>;
+  }
+};
+
 const Button = ({ clickHandler, text }) => {
   return <button onClick={clickHandler}>{text}</button>;
 };
@@ -23,9 +47,6 @@ function App() {
       setter(current + 1);
     };
   };
-  let all = good + neutral + bad;
-  let nonZeroSum = all === 0 ? 1 : all;
-  let positive = good / nonZeroSum;
 
   return (
     <div>
@@ -34,15 +55,7 @@ function App() {
       <Button clickHandler={update(setNeutral, neutral)} text={"neutral"} />
       <Button clickHandler={update(setBad, bad)} text={"bad"} />
       <h1>statistics</h1>
-      <Result text={"good"} total={good} />
-      <Result text={"neutral"} total={neutral} />
-      <Result text={"bad"} total={bad} />
-      <Result text={"all"} total={all} />
-      <Result
-        text={"all"}
-        total={(good * 1 + neutral * 0 + bad * -1) / nonZeroSum}
-      />
-      <Result text={"positive"} total={positive} />
+      <Statistics feedback={{ good, neutral, bad }} />
     </div>
   );
 }
