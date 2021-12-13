f03e398aad7e4aa2818708246f8ad1a866201ccf
benjaminhonorio <benjaminhonorio.12@gmail.com>
2021-12-12 21:47:02 -0500

    Phonebook step6 done
    useEffect to fetch data from json
    
    json-server to serve fake data from json file

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const people = [
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ];
 
-  const [persons, setPersons] = useState(people); // people to be shown in the phonebook
-  const [allPeople, setAllPeople] = useState(people); // keeps all people including newly added
+  const [persons, setPersons] = useState([]);
+  const [allPeople, setAllPeople] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchValue, setSearchValue] = useState("");
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setAllPeople(response.data);
+      setPersons(response.data);
+    });
+  };
+  useEffect(hook, []);
 
   const addNewPerson = (event) => {
     event.preventDefault();
